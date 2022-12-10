7199143d33b5cc33164ae93339e5db6f72b68fec
Gülsüm Baştan <gulsumbastan@gmail.com>
2022-12-11 00:43:54 +0300

    update

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { CarResponseModel } from '../models/carResponseModel';
+import { Car } from '../models/car';
+import { listResponseModel } from '../models/listResponseModel';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +13,8 @@ export class CarService {
   apiUrl = 'https://localhost:44327/api/Cars/getall';
 
   constructor(private httpClient: HttpClient) { }
-  getCars():Observable<CarResponseModel> {
-    return this.httpClient.get<CarResponseModel>(this.apiUrl);
+  getCars():Observable<listResponseModel<Car>> {
+    return this.httpClient.get<listResponseModel<Car>>(this.apiUrl);
 
 
   }
